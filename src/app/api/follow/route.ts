eab226f4b82b97a27eb536317b2947cdea39928f
Liken Tan <likendev@gmail.com>
2023-11-13 16:58:40 +0800

    feat: follower system API

---
+++ src/app/api/follow/route.ts
@@ -0,0 +1,44 @@
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(request: Request) {
+  const session = await getServerSession(authOptions);
+  const currentUserEmail = session?.user?.email!;
+  const { targetUserId } = await request.json();
+
+  const currentUserId = await prisma.user
+    .findUnique({ where: { email: currentUserEmail } })
+    .then((user) => user?.id);
+
+  const record = await prisma.follows.create({
+    data: {
+      followerId: currentUserId,
+      followingId: targetUserId,
+    },
+  });
+
+  return NextResponse.json(record);
+}
+
+export async function DELETE(request: NextRequest) {
+  const session = await getServerSession(authOptions);
+  const currentUserEmail = session?.user?.email!;
+  const { targetUserId } = await request.json();
+
+  const currentUserId = await prisma.user
+    .findUnique({ where: { email: currentUserEmail } })
+    .then((user) => user?.id);
+
+  const record = await prisma.follows.delete({
+    where: {
+      followerId_followingId: {
+        followerId: currentUserId,
+        followingId: targetUserId,
+      },
+    },
+  });
+
+  return NextResponse.json(record);
+}
